9606d0b3b490405aab880f0b542b600e54249549
Danilo Arruda <da.ni.lo_arruda@outlook.com>
2022-01-23 16:19:14 -0300

    fix app initialization

--- src/main/java/com/danilo/barbershop/BarbershopApplication.java
+++ src/main/java/com/danilo/barbershop/BarbershopApplication.java
@@ -2,8 +2,9 @@ package com.danilo.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class BarbershopApplication {
 
 	public static void main(String[] args) {
